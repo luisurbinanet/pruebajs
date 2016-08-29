40cd26084ef6b4d24a476f5677350b33cc6eda2a
Luis C. Urbina G <luis@luisurbina.net>
2016-08-29 17:43:55 -0400

    Primer calendario Operativo

--- js/logic.js
+++ js/logic.js
@@ -1,20 +1,13 @@
-		// On Load of the window
-		window.onload = function() {
-			
-			// Start calendar
-			var c = new Calendar("divCalendar");			
-			c.showCurrent();
-			
-			// Bind next and previous button clicks
-			getId('btnNext').onclick = function() {
-				c.nextMonth();
-			};
-			getId('btnPrev').onclick = function() {
-				c.previousMonth();
-			};
-		}
-		
-		// Get element by id
-		function getId(id) {
-			return document.getElementById(id);
-		}
+/**
+ *
+ *
+ *
+ *
+ */
+
+function makeMyCalendars() {
+    // Start calendar
+    var c = new Calendar("divCalendars");
+    c.showCurrent();
+
+}
